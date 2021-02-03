23f9d6ff9d33fe11ffec039da670a35dfcb1f08f
Polina-mind <popova.v.polina@gmail.com>
2021-02-03 19:07:19 +0200

    update

--- src/js/localstorage.js
+++ src/js/localstorage.js
@@ -5,28 +5,33 @@ const refs = {
 
 refs.changeTheme.addEventListener('change', handleTheme);
 
-const currentTheme = localStorage.getItem('theme');
-refs.body.classList.add(currentTheme);
+refs.body.classList.add(localStorage.getItem('theme'));
 
 function handleTheme(event) {
   event.preventDefault();
 
+  const currentTheme = localStorage.getItem('theme');
+
   if (currentTheme === 'light-theme') {
-    refs.body.classList.replace('light-theme', 'dark-theme');
+    refs.body.classList.replace(`${currentTheme}`, 'dark-theme');
     localStorage.setItem('theme', 'dark-theme');
     refs.changeTheme.setAttribute('checked', 'true');
+    console.log(localStorage);
   } else {
-    refs.body.classList.replace('dark-theme', 'light-theme');
+    refs.body.classList.replace(`${currentTheme}`, 'light-theme');
     localStorage.setItem('theme', 'light-theme');
-    refs.changeTheme.removeAttribute('checked');
+    refs.changeTheme.setAttribute('checked', 'false');
+    console.log(localStorage);
   }
 }
+
+//Вариант 2
 
 // const Theme = {
 //   LIGHT: 'light-theme',
 //   DARK: 'dark-theme',
 // };
 
-// localStorage.setItem('Theme', JSON.stringify(Theme));
+// localStorage.setItem('Theme', JSON.stringify(Theme.LIGHT));
 
 // console.log(localStorage.getItem('Theme'));
